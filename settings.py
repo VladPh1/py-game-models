823229e40590c9b15743421f593fd1271a0572ba
Vladyslav Pihol <freepgvlad@gmail.com>
2025-07-29 18:26:17 +0300

    Solution3

--- settings.py
+++ settings.py
@@ -1,13 +1,7 @@
 import os
-from logging import DEBUG
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-ALLOWED_HOSTS = ["localhost"]
-
-if DEBUG is False:
-    pass
 
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
